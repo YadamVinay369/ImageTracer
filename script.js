6b431e8e1a5cdefcbff70441cdbcd0c63701cf92
YadamVinay369 <vinay.y21@iiits.in>
2024-07-19 23:00:51 +0530

    added search functionalities

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const imagesWrapper = document.querySelector(".images");
 const loadMoreImagesBtn = document.querySelector(".load-more");
+const searchInput = document.querySelector(".search-box input")
 
 const apiKey = "UflDnIGlobFLmPAvE6J8eTEDrmBdP2eJEaM7F74H4yvdu1fhw9iwCv3v";
 const perPage = 15;
 let currentPage = 1;
+let searchTerm = null;
 
 const generateHTML = (images) => {
   imagesWrapper.innerHTML += images
@@ -34,16 +36,26 @@ const getImages = (apiURL) => {
       generateHTML(data.photos);
       loadMoreImagesBtn.innerText = "Load More";
       loadMoreImagesBtn.classList.remove("disabled");
-    });
+    }).catch(()=>alert("Failed to load images"));
 };
 
 const loadMoreImages = () => {
   currentPage++;
   let apiURL = `https://api.pexels.com/v1/curated?page=${currentPage}&per_page=${perPage}`;
+  apiURL = searchTerm ? `https://api.pexels.com/v1/search?query=${searchTerm}&page=${currentPage}&per_page=${perPage}` : apiURL
   getImages(apiURL);
 };
 
-getImages(
-  `https://api.pexels.com/v1/curated?page=${currentPage}&per_page=${perPage}`
-);
+const loadSearchImages = (e) =>{
+    if(e.target.value === "") return searchTerm = null;
+    if(e.key === "Enter"){
+        searchTerm = e.target.value
+        currentPage = 1
+        imagesWrapper.innerHTML = ""
+        getImages(`https://api.pexels.com/v1/search?query=${searchTerm}&page=${currentPage}&per_page=${perPage}`)
+    }
+}
+
+getImages(`https://api.pexels.com/v1/curated?page=${currentPage}&per_page=${perPage}`);
 loadMoreImagesBtn.addEventListener("click", loadMoreImages);
+searchInput.addEventListener("keyup",loadSearchImages)
